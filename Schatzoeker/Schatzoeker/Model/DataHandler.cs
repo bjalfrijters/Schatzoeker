e9c92009855946e5c0971c682583f162a6737bcf
Gerjan <gerjanholsappel@hotmail.com>
2015-01-20 15:09:59 +0100

    een check gemaakt voor de database aanmaken/uitlezen en database aangevult met een langere lijst aan waypoints

--- Schatzoeker/Schatzoeker/Model/DataHandler.cs
+++ Schatzoeker/Schatzoeker/Model/DataHandler.cs
@@ -24,6 +24,8 @@ namespace Schatzoeker.Model
         public DataHandler(string inputFile)
         {
             dbConnection = String.Format("Data Source={0}", inputFile);
+            setupDataBase();
+
         }
 
         /// <summary>
@@ -42,7 +44,23 @@ namespace Schatzoeker.Model
         {
             dbConnection = String.Format("Data Source={0}", inputFile);
             if (construct)
-            setupDataBase();
+                resetDatabase();
+        }
+
+        private void resetDatabase()
+        {
+            try
+            {
+                SQLiteConnection cnn = new SQLiteConnection(dbConnection);
+                cnn.DropTable<Waypoint>();
+                cnn.DropTable<Puzzle>();
+                cnn.Close();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            fillDataBase();
         }
 
         
@@ -51,7 +69,7 @@ namespace Schatzoeker.Model
             try
             {
                 SQLiteConnection cnn = new SQLiteConnection(dbConnection);
-                cnn.DropTable<Waypoint>();
+                //cnn.DropTable<Waypoint>();
                 cnn.Query<Waypoint>(@"CREATE TABLE IF NOT EXISTS
                                 waypoint (Id      INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                                             description    VARCHAR( 140 ),
@@ -82,6 +100,10 @@ namespace Schatzoeker.Model
                 throw new Exception(e.Message);
             }
 
+            
+            List<Puzzle> check = new List<Puzzle>();
+            check = getAllPuzzles();
+            if (check.Count==0)
             fillDataBase();
         }
 
@@ -97,8 +119,48 @@ namespace Schatzoeker.Model
             Waypoints.Add(new Waypoint("1e bocht Valkenberg", new Geopoint(new BasicGeoposition() { Latitude = 51.59265, Longitude = 4.77844 })));
             Waypoints.Add(new Waypoint("2e bocht Valkenberg", new Geopoint(new BasicGeoposition() { Latitude = 51.59258, Longitude = 4.77806 })));
             Waypoints.Add(new Waypoint("Einde park", new Geopoint(new BasicGeoposition() { Latitude = 51.59059, Longitude = 4.77707 })));
-
-
+            Waypoints.Add(new Waypoint("Kasteel van Breda", new Geopoint(new BasicGeoposition() { Latitude = 51.59061, Longitude = 4.77624 })));
+            Waypoints.Add(new Waypoint("Stadhouderspoort", new Geopoint(new BasicGeoposition() { Latitude = 51.58992, Longitude = 4.77634 })));
+            Waypoints.Add(new Waypoint("Kruising Kasteelplein/Cingelstraat", new Geopoint(new BasicGeoposition() { Latitude = 51.59033, Longitude = 4.77623 })));
+            Waypoints.Add(new Waypoint("Huis van Brecht", new Geopoint(new BasicGeoposition() { Latitude = 51.59043, Longitude = 4.77518 })));
+            Waypoints.Add(new Waypoint("2e bocht Cingelstraat", new Geopoint(new BasicGeoposition() { Latitude = 51.59000, Longitude = 4.77429 })));
+            Waypoints.Add(new Waypoint("Spanjaardsgat", new Geopoint(new BasicGeoposition() { Latitude = 51.59010, Longitude = 4.77336 })));
+            Waypoints.Add(new Waypoint("Vismarkt", new Geopoint(new BasicGeoposition() { Latitude = 51.58982, Longitude = 4.77321 })));
+            Waypoints.Add(new Waypoint("Havermarkt", new Geopoint(new BasicGeoposition() { Latitude = 51.58932, Longitude = 4.77444 })));
+            Waypoints.Add(new Waypoint("Driehoek Kerkplein 1", new Geopoint(new BasicGeoposition() { Latitude = 51.58872, Longitude = 4.77501 })));
+            Waypoints.Add(new Waypoint("Grote of Onze Lieve Vrouwekerk", new Geopoint(new BasicGeoposition() { Latitude = 51.58878, Longitude = 4.77549 })));
+            Waypoints.Add(new Waypoint("Driehoek Kerkplein 3", new Geopoint(new BasicGeoposition() { Latitude = 51.58864, Longitude = 4.77501 })));
+            Waypoints.Add(new Waypoint("Het poortje", new Geopoint(new BasicGeoposition() { Latitude = 51.58822, Longitude = 4.77525 })));
+            Waypoints.Add(new Waypoint("Ridderstraat", new Geopoint(new BasicGeoposition() { Latitude = 51.58716, Longitude = 4.77582 })));
+            Waypoints.Add(new Waypoint("Grote Markt", new Geopoint(new BasicGeoposition() { Latitude = 51.58747, Longitude = 4.77662 })));
+            Waypoints.Add(new Waypoint("Het Wit Lam", new Geopoint(new BasicGeoposition() { Latitude = 51.58771, Longitude = 4.77652 })));
+            Waypoints.Add(new Waypoint("Bevrijdingsmonument", new Geopoint(new BasicGeoposition() { Latitude = 51.58797, Longitude = 4.77638 })));
+            Waypoints.Add(new Waypoint("Stadshuis", new Geopoint(new BasicGeoposition() { Latitude = 51.58885, Longitude = 4.77616 })));
+            Waypoints.Add(new Waypoint("Kruising Grote Markt / Stadserf", new Geopoint(new BasicGeoposition() { Latitude = 51.58883, Longitude = 4.77617 })));
+            Waypoints.Add(new Waypoint("Achterkant stadshuis", new Geopoint(new BasicGeoposition() { Latitude = 51.58889, Longitude = 4.77659 })));
+            Waypoints.Add(new Waypoint("Kruising Grote Markt / Stadserf (terug gaan)", new Geopoint(new BasicGeoposition() { Latitude = 51.58883, Longitude = 4.77617 })));
+            Waypoints.Add(new Waypoint("Terug naar begin Grote Markt", new Geopoint(new BasicGeoposition() { Latitude = 51.58747, Longitude = 4.77662 })));
+            Waypoints.Add(new Waypoint("Antonius van Paduakerk", new Geopoint(new BasicGeoposition() { Latitude = 51.58761, Longitude = 4.77712 })));
+            Waypoints.Add(new Waypoint("Kruising St. Jansstraat / Molenstraat", new Geopoint(new BasicGeoposition() { Latitude = 51.58828, Longitude = 4.77858 })));
+            Waypoints.Add(new Waypoint("Bibliotheek", new Geopoint(new BasicGeoposition() { Latitude = 51.58773, Longitude = 4.77948 })));
+            Waypoints.Add(new Waypoint("Kruising Molenstraat / Kloosterplein", new Geopoint(new BasicGeoposition() { Latitude = 51.58752, Longitude = 4.77994 })));
+            Waypoints.Add(new Waypoint("Kloosterkazerne", new Geopoint(new BasicGeoposition() { Latitude = 51.58794, Longitude = 4.78105 })));
+            Waypoints.Add(new Waypoint("Chasse Theater", new Geopoint(new BasicGeoposition() { Latitude = 51.58794, Longitude = 4.78218 })));
+            Waypoints.Add(new Waypoint("1e bocht Kloosterplein / Begin Vlaszak", new Geopoint(new BasicGeoposition() { Latitude = 51.58794, Longitude = 4.78105 })));
+            Waypoints.Add(new Waypoint("Binding van Isaäc", new Geopoint(new BasicGeoposition() { Latitude = 51.58862, Longitude = 4.78079 })));
+            Waypoints.Add(new Waypoint("Einde Vlaszak / Begin Boschstraat", new Geopoint(new BasicGeoposition() { Latitude = 51.58955, Longitude = 4.78038 })));
+            Waypoints.Add(new Waypoint("Beyerd", new Geopoint(new BasicGeoposition() { Latitude = 51.58966, Longitude = 4.78076 })));
+            Waypoints.Add(new Waypoint("Gasthuispoort", new Geopoint(new BasicGeoposition() { Latitude = 51.58939, Longitude = 4.77982 })));
+            Waypoints.Add(new Waypoint("2e bocht Veemarktstraat", new Geopoint(new BasicGeoposition() { Latitude = 51.58905, Longitude = 4.77981 })));
+            Waypoints.Add(new Waypoint("Kruising St. Annastraat / Veemarktstraat", new Geopoint(new BasicGeoposition() { Latitude = 51.58846, Longitude = 4.77830 })));
+            Waypoints.Add(new Waypoint("Willem Merkxtuin", new Geopoint(new BasicGeoposition() { Latitude = 51.58905, Longitude = 4.77801 })));
+            Waypoints.Add(new Waypoint("Binnen Willem Merkxtuin", new Geopoint(new BasicGeoposition() { Latitude = 51.58918, Longitude = 4.77841 })));
+            Waypoints.Add(new Waypoint("Uitgang Willem Merkxtuin", new Geopoint(new BasicGeoposition() { Latitude = 51.58905, Longitude = 4.77801 })));
+            Waypoints.Add(new Waypoint("Kruising Catharinastraat / St. Annastraat", new Geopoint(new BasicGeoposition() { Latitude = 51.58960, Longitude = 4.77770 })));
+            Waypoints.Add(new Waypoint("Begijnenhof", new Geopoint(new BasicGeoposition() { Latitude = 51.58965, Longitude = 4.77830 })));
+            Waypoints.Add(new Waypoint("Binnen Begijnenhof", new Geopoint(new BasicGeoposition() { Latitude = 51.58997, Longitude = 4.77810 })));
+            Waypoints.Add(new Waypoint("Uitgang Begijnenhof", new Geopoint(new BasicGeoposition() { Latitude = 51.58965, Longitude = 4.77830 })));
+            Waypoints.Add(new Waypoint("Eindpunt stadswandeling", new Geopoint(new BasicGeoposition() { Latitude = 51.58950, Longitude = 4.77649 })));
 
             List<Puzzle> puzzles = new List<Puzzle>();
             puzzles.Add(new Puzzle("testing"));
@@ -108,6 +170,12 @@ namespace Schatzoeker.Model
             puzzles.Add(new Puzzle("pennenbakje"));
             puzzles.Add(new Puzzle("schoen"));
             puzzles.Add(new Puzzle("muts"));
+            puzzles.Add(new Puzzle("laptop"));
+            puzzles.Add(new Puzzle("deurbel"));
+            puzzles.Add(new Puzzle("telefoon"));
+            puzzles.Add(new Puzzle("boterhamzakeje"));
+            puzzles.Add(new Puzzle("tas"));
+            puzzles.Add(new Puzzle("deurmat"));
 
             try
             {
